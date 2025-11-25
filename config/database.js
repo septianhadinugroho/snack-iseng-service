bcb7470351fdcb267080cccd2a3aa1444b83f84b
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-11-25 15:22:34 +0700

    feat: load PostgreSQL driver manually and enforce its usage in all environments

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const pg = require('pg'); // <--- TAMBAHAN 1: Load driver pg manual
 
 module.exports = {
   development: {
@@ -7,18 +8,21 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    dialectModule: pg // <--- Paksa dev pake pg
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectModule: pg // <--- Paksa test pake pg
   },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectModule: pg, // <--- TAMBAHAN 2: INI WAJIB BUAT VERCEL
     dialectOptions: {
       ssl: {
         require: true,
